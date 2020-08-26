42a9f0eae515db4f32eb47f89b4c32e5b29e3836
Luis Gustavo <48425588+lluisgustavo@users.noreply.github.com>
2020-08-26 11:35:23 -0300

    Add files via upload

---
+++ sklearn_transformers.py
@@ -0,0 +1,36 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class DropColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data.drop(labels=self.columns, axis='columns')
+
+#Columns Transformer
+
+class Balanceamento(BaseEstimator, TransformerMixin):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+        
+    
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X, y):
+        from imblearn.over_sampling import SMOTE
+        smote = SMOTE(sampling_strategy='all')
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        self.X, self.y = smote.fit_resample(X,y)
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return self.X, self.y
+    
